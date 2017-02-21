081eb035c9762156b03690bf575044d311eecc60
kirimoch <87.5kei@gmail.com>
2017-02-21 10:40:41 +0900

    mochi STG

---
+++ Assets/Tentacle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacle : StageObject {
+
+    public float hitPoint;
+    public float score = 100f;
+
+    ScoreText scoreText;
+    GameManager gm;
+    GameObject objPlayer;
+
+    // Use this for initialization
+    void Start () {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (objPlayer == null)
+        {
+            objPlayer = gm.player;
+        }
+        if(isActive)
+        {
+
+        }
+
+    }
+}
